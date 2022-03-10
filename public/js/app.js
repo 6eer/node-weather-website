824ead1ef60419129874a97b81a542c8cea7e11e
6eer <@email>
2022-03-09 23:31:47 -0300

    Minor fixes

--- public/js/app.js
+++ public/js/app.js
@@ -6,6 +6,7 @@ const messageTwo = document.querySelector('#message-2')
 weatherForm.addEventListener('submit', (e) => {
     e.preventDefault()
     const address = search.value
+    search.value = ''
     const url = '/weather?address=' + address
 
     messageOne.textContent = 'Loading...'
